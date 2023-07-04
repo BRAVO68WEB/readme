08be35b783d5e0f013208d834f617be348ff8cee
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-05 00:46:18 +0530

    ui: updated dashbaord sidebar

--- apps/client/components/ui/DashboardLayoutContainer/index.tsx
+++ apps/client/components/ui/DashboardLayoutContainer/index.tsx
@@ -25,11 +25,6 @@ const navigation = [
     { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
     { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
 ];
-const teams = [
-    { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-    { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-    { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-];
 
 interface DashboardLayoutContainerProps {
     children: React.ReactNode;
@@ -40,7 +35,7 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
 
     return (
         <>
-            <div>
+            <div className={"flex w-screen"}>
                 <Transition.Root show={sidebarOpen} as={Fragment}>
                     <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
                         <Transition.Child
@@ -65,7 +60,7 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
                                 leaveFrom="translate-x-0"
                                 leaveTo="-translate-x-full"
                             >
-                                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                                <Dialog.Panel className="relative mr-16 flex flex-1">
                                     <Transition.Child
                                         as={Fragment}
                                         enter="ease-in-out duration-300"
@@ -129,36 +124,6 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
                                                         ))}
                                                     </ul>
                                                 </li>
-                                                <li>
-                                                    <div className="text-xs font-semibold leading-6 text-gray-400">
-                                                        Your teams
-                                                    </div>
-                                                    <ul
-                                                        role="list"
-                                                        className="-mx-2 mt-2 space-y-1"
-                                                    >
-                                                        {teams.map(team => (
-                                                            <li key={team.name}>
-                                                                <a
-                                                                    href={team.href}
-                                                                    className={cn(
-                                                                        team.current
-                                                                            ? "bg-gray-800 text-white"
-                                                                            : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                                                                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                                                                    )}
-                                                                >
-                                                                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                                                        {team.initial}
-                                                                    </span>
-                                                                    <span className="truncate">
-                                                                        {team.name}
-                                                                    </span>
-                                                                </a>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </li>
                                                 <li className="mt-auto">
                                                     <a
                                                         href="#"
@@ -181,7 +146,7 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
                 </Transition.Root>
 
                 {/* Static sidebar for desktop */}
-                <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+                <div className="hidden h-screen lg:inset-y-0 lg:z-50 lg:flex  lg:flex-col">
                     {/* Sidebar component, swap this element with another sidebar if you like */}
                     <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
                         <div className="flex h-16 shrink-0 items-center">
@@ -203,39 +168,13 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
                                                         item.current
                                                             ? "bg-gray-800 text-white"
                                                             : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                                                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                                                        "group flex flex justify-center gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
                                                     )}
                                                 >
                                                     <item.icon
                                                         className="h-6 w-6 shrink-0"
                                                         aria-hidden="true"
                                                     />
-                                                    {item.name}
-                                                </a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </li>
-                                <li>
-                                    <div className="text-xs font-semibold leading-6 text-gray-400">
-                                        Your teams
-                                    </div>
-                                    <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                        {teams.map(team => (
-                                            <li key={team.name}>
-                                                <a
-                                                    href={team.href}
-                                                    className={cn(
-                                                        team.current
-                                                            ? "bg-gray-800 text-white"
-                                                            : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                                                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                                                    )}
-                                                >
-                                                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                                        {team.initial}
-                                                    </span>
-                                                    <span className="truncate">{team.name}</span>
                                                 </a>
                                             </li>
                                         ))}
@@ -244,13 +183,12 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
                                 <li className="mt-auto">
                                     <a
                                         href="#"
-                                        className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
+                                        className="group -mx-2 flex flex justify-center gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
                                     >
                                         <Cog6ToothIcon
                                             className="h-6 w-6 shrink-0"
                                             aria-hidden="true"
                                         />
-                                        Settings
                                     </a>
                                 </li>
                             </ul>
@@ -258,8 +196,8 @@ export default function DashboardLayoutContainer({ children }: DashboardLayoutCo
                     </div>
                 </div>
 
-                <div className="lg:pl-72">
-                    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:hidden lg:px-8">
+                <div className="w-full">
+                    <div className="sticky top-0 z-40 flex h-16 w-full shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:hidden lg:px-8">
                         <button
                             type="button"
                             className="-m-2.5 p-2.5 text-gray-700"
