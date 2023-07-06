b3471cf6216ac16ea0e5d33d212566bd79d6460a
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-07 02:52:09 +0530

    meta: mappings postlists from an array

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,12 +1,33 @@
 import Posts from "@/components/Posts";
 import Navbar from "@/components/ui/Navbar";
 
+const posts = [
+    {
+        id: 1,
+        title: "How I Deployed on Vercel",
+        date: "23/4/2056",
+        img: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+        id: 2,
+        title: "How I Deployed on Vercel",
+        date: "23/4/2056",
+        img: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+        id: 3,
+        title: "How I Deployed on Vercel",
+        date: "23/4/2056",
+        img: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+];
+
 export default function Home() {
     return (
         <>
             <Navbar />
             <main className="items-center justify-between bg-background p-24">
-                <Posts />
+                <Posts posts={posts} />
             </main>
         </>
     );
--- apps/client/components/Posts/index.tsx
+++ apps/client/components/Posts/index.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 
-interface PostsProps {}
+interface PostsProps {
+    posts: { id: number; title: string; date: string; img: string }[];
+}
 
-const Posts = (props: PostsProps) => {
+const Posts = ({ posts }: PostsProps) => {
     return (
         <div
             className={
-                "grid grid-cols-1 gap-2 p-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+                "grid grid-cols-1 gap-3 p-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
             }
         >
-            <div className="post flex aspect-square w-full flex-col justify-between rounded-xl bg-yellow-300 p-6">
-                <h2 className={"text-5xl font-bold"}>How I Deployed on Vercel</h2>
-                <div className="flex w-full items-baseline justify-between">
-                    <img
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        className={
-                            "h-10 w-10 overflow-hidden rounded-full object-cover object-center"
-                        }
-                        alt=""
-                    />
-                    <p className={"text-xl font-bold"}>on 23/4/2056</p>
+            {posts.map(post => (
+                <div
+                    key={post.id}
+                    className="post flex aspect-square w-full flex-col justify-between rounded-xl bg-yellow-300 p-6"
+                >
+                    <h2 className={"text-5xl font-bold"}>How I Deployed on Vercel</h2>
+                    <div className="flex w-full items-baseline justify-between">
+                        <img
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            className={
+                                "h-10 w-10 overflow-hidden rounded-full object-cover object-center"
+                            }
+                            alt=""
+                        />
+                        <p className={"text-xl font-bold"}>on 23/4/2056</p>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
