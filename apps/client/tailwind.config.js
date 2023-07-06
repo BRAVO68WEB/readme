9494e63625d3d102f657bd74e2f935758a76b7cc
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-07 02:23:23 +0530

    ui: updated color theme

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,20 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {
+            maxWidth: {
+                "8xl": "90rem",
+            },
             colors: {
-                primary: "rgb(var(--color-primary) / <alpha-value>)",
+                primary: colors.indigo["600"],
                 secondary: "rgb(var(--color-secondary) / <alpha-value>)",
                 accent: "rgb(var(--color-accent) / <alpha-value>)",
                 text: "rgb(var(--color-text) / <alpha-value>)",
-                background: "rgb(var(--color-background) / <alpha-value>)",
-            }
-        }
+                background: colors.gray["900"],
+            },
+        },
     },
-    plugins: [require('@tailwindcss/forms')],
-}
+    plugins: [require("@tailwindcss/forms")],
+};
